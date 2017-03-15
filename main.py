a1bcd0f3135421cfd317f3fd0fda69df3472197e
Haley Zeng <hzeng@stuy.edu>
2017-03-15 11:02:27 -0400

    yeah

---
+++ main.py
@@ -0,0 +1,26 @@
+from display import *
+from draw import *
+from parser import *
+from matrix import *
+import math
+
+screen = new_screen()
+color = [ 0, 255, 0 ]
+edges = []
+transform = new_matrix()
+
+# print_matrix( make_translate(3, 4, 5) )
+# print
+# print_matrix( make_scale(3, 4, 5) )
+# print
+# print_matrix( make_rotX(math.pi/4) )
+# print
+# print_matrix( make_rotY(math.pi/4) )
+# print
+# print_matrix( make_rotZ(math.pi/4) )
+
+add_circle(edges, 250, 250, 0, 100, 9)
+draw_lines(edges, screen, color)
+display(screen)
+
+#parse_file( 'script', edges, transform, screen, color )
